df464a1f145968d4c0b05eb033f1c50f900e733f
aaug <aaug@caasco.ca>
2017-04-21 09:58:33 -0400

    code cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,20 @@
 // Include gulp
 var gulp = require('gulp'),
-    // Include Our Plugins
-    jshint = require('gulp-jshint'),
-    sass = require('gulp-ruby-sass'),
-    prettify = require('gulp-prettify'),
-    sourcemaps = require('gulp-sourcemaps'),
-    mmq = require('gulp-merge-media-queries'),
-    notify = require('gulp-notify'),
-    copy = require('copy'),
-    livereload = require('gulp-livereload');
+  // Include Our Plugins
+  copy = require('copy'),
+  imgmin = require('gulp-imagemin'),
+  jshint = require('gulp-jshint'),
+  livereload = require('gulp-livereload'),
+  mmq = require('gulp-merge-media-queries'),
+  notify = require('gulp-notify'),
+  prettify = require('gulp-prettify'),
+  sass = require('gulp-ruby-sass'),
+  sourcemaps = require('gulp-sourcemaps');
 
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('CODE/src/**/*.js')
+  return gulp.src('CODE/src/**/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
 });
@@ -21,65 +22,83 @@ gulp.task('lint', function() {
 
 //Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('CODE/src/**/*.js')
+  return gulp.src('CODE/src/**/*.js')
     // .pipe(prettify({indent_size: 2}))
     .pipe(gulp.dest('CODE/dist/'))
     .pipe(livereload())
-    .pipe(notify({ message: 'Scripts task complete' }));
+    .pipe(notify({
+      message: 'Scripts task complete'
+    }));
 });
 
 
 // Compile Our Sass
 gulp.task('styles', function() {
-    return sass('CODE/src/**/*.scss', { style: 'expanded' })
+  return sass('CODE/src/**/*.scss', {
+      style: 'expanded'
+    })
     .on('error', sass.logError)
     .pipe(gulp.dest('CODE/src/'))
     .pipe(livereload())
-    .pipe(notify({ message: 'Styles task complete' }));
+    .pipe(notify({
+      message: 'Styles task complete'
+    }));
 });
 
 
 //Merge Media Queries
-gulp.task('mmq', function () {
-    return gulp.src('CODE/src/css/*.css')
+gulp.task('mmq', function() {
+  return gulp.src('CODE/src/css/*.css')
     .pipe(sourcemaps.init())
     .pipe(mmq({
-        log: true
+      log: true
     }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest('CODE/dist/css/'))
     .pipe(livereload())
-    .pipe(notify({ message: 'Merge complete' }));
+    .pipe(notify({
+      message: 'Merge complete'
+    }));
 });
 
 
 // HTML Task
 gulp.task('prettify', function() {
-    gulp.src('CODE/src/**/*.html')
-    .pipe(prettify({indent_size: 2}))
+  gulp.src('CODE/src/**/*.html')
+    .pipe(prettify({
+      indent_size: 2
+    }))
     .pipe(gulp.dest('CODE/dist'))
-    .pipe(notify({ message: 'HTML task complete' }));
+    .pipe(notify({
+      message: 'HTML task complete'
+    }));
 });
 
 
-// copy Task
-gulp.task('copy-img', function (cb) {
-    copy('CODE/src/img/*.{png,gif,jpg,svg}', './CODE/dist/img', cb);
-});
+// optimization Task
+gulp.task('imgmin', () =>
+  gulp.src('CODE/src/img/*.{png,gif,jpg,svg}')
+  .pipe(imgmin([
+    imgmin.optipng({
+      optimizationLevel: 5
+    })
+  ]))
+  .pipe(gulp.dest('./CODE/dist/img'))
+);
 
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('CODE/src/**/*.html', ['prettify'])
-    gulp.watch('CODE/src/**/*.js', ['lint', 'scripts'])
-    gulp.watch('CODE/src/**/*.scss', ['styles'])
-    gulp.watch('CODE/src/**/*.css', ['mmq'])
-    gulp.watch('CODE/src/img/', ['copy-img'])
-    // Create LiveReload server
-    livereload.listen()
-    // Watch any files in dist/, reload n change
-    gulp.watch(['CODE/dist/**']).on('change', livereload.changed);
+  gulp.watch('CODE/src/**/*.html', ['prettify'])
+  gulp.watch('CODE/src/**/*.js', ['lint', 'scripts'])
+  gulp.watch('CODE/src/**/*.scss', ['styles'])
+  gulp.watch('CODE/src/**/*.css', ['mmq'])
+  gulp.watch('CODE/src/img/', ['imgmin'])
+  // Create LiveReload server
+  livereload.listen()
+  // Watch any files in dist/, reload n change
+  gulp.watch(['CODE/dist/**']).on('change', livereload.changed);
 });
 
 // Default Task
-gulp.task('default', ['lint', 'prettify','styles',  'scripts', 'mmq', 'copy-img', 'watch']); //
+gulp.task('default', ['lint', 'prettify', 'styles', 'scripts', 'mmq', 'imgmin', 'watch']); //
