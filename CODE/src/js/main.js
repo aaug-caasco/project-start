7adc3eb4eed108b59e25da3e71363778e6489ea6
aaug <aaug@caasco.ca>
2017-05-16 09:46:36 -0400

    updating script

---
+++ CODE/src/js/main.js
@@ -0,0 +1,28 @@
+window.projectName = window.projectName || {};
+
+projectName = {
+
+    init : function () {
+
+      projectName.namespace1();
+      projectName.namespace2();
+      projectName.namespace3();
+
+    },
+
+
+    namespace1: function () { console.log("this is namespace1"); },
+
+
+
+    namespace2: function () { console.log("this is namespace2"); },
+
+
+
+    namespace3: function () { console.log("this is namespace3"); }
+
+};
+
+$(document).ready(function() { projectName.init(); });
+
+// ..............................................................................
